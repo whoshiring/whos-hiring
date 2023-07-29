3cb2bba25a9aefc43249e12c7c38c99921c35966
George Bockari <bitwhys@gmail.com>
2023-07-28 21:23:34 -0400

    chore: everything works

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,12 +1,12 @@
-import { prisma } from "database";
+import { prisma } from "@whoshiring/database"
 
 export default async function IndexPage() {
-  const users = await prisma.user.findMany();
+  const users = await prisma.user.findMany()
 
   return (
     <div>
       <h1>Hello World</h1>
       <pre>{JSON.stringify(users, null, 2)}</pre>
     </div>
-  );
+  )
 }
